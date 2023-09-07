ea9b406b461680af9bcfda3597206d97d6f35bf6
John Doe <civi.pranav@gmail.com>
2023-09-07 18:59:00 -0500

    animating stars, animating introduction text, css toggle, tweaking some css elements

--- stars.js
+++ stars.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function() {
     document.getElementById("mainStyleSheet").setAttribute("href", localStorage.getItem("storageFile"));
 
     var styleToggle = document.getElementById("style-toggle-card");
     styleToggle.addEventListener("click", toggleStyleSheet);
 
+    // document.querySelector("style-toggle-card").addEventListener("click", toggleStyleSheet);
     const nightSky = document.querySelector(".night-sky");
+    const textContainer = document.querySelector(".text.first-text");
 
     // generate a random star with a random location and set animation delay
     function createRandomStar() {
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
         star.className = "star";
         star.style.left = `${Math.random() * 100}vw`;
         star.style.top = `${Math.random() * 100}vh`;
+        star.style.animationDelay = `${Math.random() * 3.0}s`;
         return star;
     }
 
@@ -23,10 +26,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // storing the various print statments in different languages
+    const p = "\"Pranav Civi\"";
+    const texts = [`console.log(${p});`, `System.out.println(${p});`, `print(${p})`, `std::cout << ${p};`, `printf(${p});`, `System.Console.WriteLine(${p});`];
+    const text = texts[Math.floor(Math.random() * 6)];
+    let charIndex = 0;
+
+    // function to type out the print statement
+    function type() {
+        if (charIndex < text.length) {
+            textContainer.textContent += text.charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 75);
+        }
+    }
+
     // calling the functions
     generateStars();
+    type();
     
-});
+};
 
 function toggleStyleSheet(){
     var element = document.getElementById("mainStyleSheet");
@@ -43,3 +62,6 @@ function toggleStyleSheet(){
     localStorage.setItem("storageFile", newFile);
     console.log(localStorage.getItem("storageFile"));
 }
+
+
+
